66fca1aee11cae80bca71bd36696d3db82b484df
jiiiyaannn <indecisiven27@gmail.com>
2024-12-11 17:56:56 +0800

    Add simplified expense list page

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'expense_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+             BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
